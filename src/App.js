20d4cb74595d6bb35cf3bffc4e09843fe7d915e1
AS <aschecht3@student.touro.edu>
2023-02-05 21:57:37 -0500

    Created personal home page using Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
-import { Button } from "@mui/material";
+import { Box,Button,  Divider, Avatar, Accordion, AccordionDetails,AccordionSummary, Card, CardMedia, Grid} from "@mui/material";
+import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-         <Button variant="contained">Click me</Button>
-      </header>
+        <Box sx={{float:"right"}}>
+          <Avatar src={process.env.PUBLIC_URL + 'pictures/blankProfile.png'} 
+           sx={{border:1.5 , width: 70, height:70}}/>
+          <Button variant="text" float="right">Sign In</Button>
+        </Box>
+       
+          <img className = "App-Logo" src= {process.env.PUBLIC_URL + 'pictures/HomeworkHelp.png'} alt="Camp Summer logo" height={300} width={500} />
+    <Divider variant="middle" light= {false}  />
+
+      </header>     
+  
+       <body className = "App-body">
+         
+          <Accordion>
+            <AccordionSummary expandIcon={<KeyboardDoubleArrowDownIcon />} >
+                <h2>Hours</h2>
+            </AccordionSummary>
+            <AccordionDetails>
+              <h4>Sunday: 12:00AM - 8:00PM</h4>
+              <h4>Monday-Thursday: 5:00PM - 10:00PM</h4>
+            </AccordionDetails>
+
+            </Accordion>
+
+            <Accordion>
+
+            <AccordionSummary
+              expandIcon={<KeyboardDoubleArrowDownIcon />} >
+              <h2 text-align='center'>Subjects</h2>
+            </AccordionSummary>
+
+            <AccordionDetails>
+              <Grid sx={{flexGrow: 1}} container spacing={4} justifyContent="center">
+                <Grid item xs={2}>
+                  <Card sx={{ maxWidth: 230}}>
+                  <CardMedia sx={{height:150}}
+                  image= {process.env.PUBLIC_URL + 'pictures/math.png'} title="math Picture"
+                />
+                  <h4>Math</h4>
+                </Card>
+                </Grid>
+                
+                <Grid item xs={2}>
+                   <Card sx={{ maxWidth: 230}}>
+                  <CardMedia sx={{height:150}}
+                   image ={process.env.PUBLIC_URL + 'pictures/science.png'} title="science Picture" 
+                />
+                <h4>Science</h4>
+                </Card>
+                </Grid>
+          
+              <Grid item xs={2}>
+                   <Card sx={{ maxWidth: 230}}>
+                  <CardMedia sx={{height:150}}
+                   image ={process.env.PUBLIC_URL + 'pictures/history.png'} title="History Picture" 
+                />
+                <h4>History</h4>
+                </Card>
+                </Grid>
+
+                <Grid item xs={2}>
+                   <Card sx={{ maxWidth: 230}}>
+                  <CardMedia sx={{height:150}}
+                   image ={process.env.PUBLIC_URL + 'pictures/programming.png'} title="programming Picture" 
+                />
+                <h4>Programming</h4>
+                </Card>
+                </Grid>
+             
+              </Grid>
+          </AccordionDetails> 
+           
+          </Accordion> 
+
+          
+           </body> 
+
     </div>
   );
 }
