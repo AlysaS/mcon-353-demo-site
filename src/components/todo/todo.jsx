25e6c5e0eff11cc10c0abf8c35b9d11ee3f7981e
AS <aschecht3@student.touro.edu>
2023-02-16 18:48:02 -0500

    added basic todo page

--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React ,{useState} from 'react';
 
 export const Todo = () => {
+    
+    //store all items of list in an Array
+   const [input, setInput] = useState(""); //empty input
+
+   //
+    const [todos,setTodos] = useState( []);
+    
+    const onInput = (event) => {console.log(event.target.value);
+    setInput(event.target.value)} ;
+    
+    const addTodo = () => {
+        //todos.push(input); //dont want to just push, want to create new array with new item
+        setTodos([...todos, input]); //add everyting from old array to new and then also add the value in the input
+        console.log(todos);
+    }
     return (
-        <> 
-            <h1>Todos</h1>
-            <p>cook for shabbos</p>
-            <p>finish homework</p>
-            <p>hang out with friends</p>
-        </>
+        
+        <div> 
+            <h1>Todos</h1> 
+           
+           <input  onInput= {onInput}  />
+           <button onClick={addTodo}>ADD</button>
+
+           {
+            //want to print every string in array as a <p> 
+            todos.map(todo => (
+            <p>
+                <input type="checkbox"/>
+                {todo}
+            </p>))
+           }
+            
+        </div>
 
     )
 }
