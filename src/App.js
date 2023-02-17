156a3bca8332245cf56226e0f70206e86a71ae08
AS <aschecht3@student.touro.edu>
2023-02-16 19:37:32 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import './App.css';
+import "./App.css";
 
-import { Home } from './components/home/home';
-import {Todo} from './components/todo/todo';
+import { Home } from "./components/home/home";
+import { Todo } from "./components/todo/todo";
+import {Header} from "./components/header/header";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { AppBar } from "@mui/material";
 
-
-function App(){
-  //call my home method
+function App() {
+  //if wanted to return just one page then return it using the code below!
   //return (<Home/>);
-  return (<Todo/>);
+  //return (<Todo/>);
+
+  return (
+    <HashRouter>
+     
+      <Header />   {/* import the header component at top of the page */}
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/todo" element={<Todo />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
